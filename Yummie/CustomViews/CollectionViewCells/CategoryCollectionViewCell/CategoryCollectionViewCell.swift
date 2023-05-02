436d58d5125891a4974441c1a41f8bafc7f208cf
kocakaysegul <aysegulkocak112@gmail.com>
2023-05-02 12:16:54 +0300

    collection view problem solved by removing homeviewcontroller connection and connect back

--- Yummie/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
+++ Yummie/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -9,6 +9,8 @@ import UIKit
 import Kingfisher
 
 class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: CategoryCollectionViewCell.self)
 
     @IBOutlet weak var categoryImageView: UIImageView!
     @IBOutlet weak var categoryTitleLbl: UILabel!
--- Yummie/Views/Home/HomeViewController.swift
+++ Yummie/Views/Home/HomeViewController.swift
@@ -9,22 +9,45 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    
+    @IBOutlet weak var categoryCollectionview: UICollectionView!
+    
+    var categories : [DishCategory] = [
+        .init(id: "id1", name: "kjfgvkdj 1", image: "https://picsum.photos/100/200"),
+        .init(id: "id1", name: "kjfgvkdj 2", image: "https://picsum.photos/100/200"),
+        .init(id: "id1", name: "kjfgvkdj 3", image: "https://picsum.photos/100/200"),
+        .init(id: "id1", name: "kjfgvkdj 4", image: "https://picsum.photos/100/200"),
+        .init(id: "id1", name: "kjfgvkdj 5", image: "https://picsum.photos/100/200")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        categoryCollectionview.delegate = self
+        categoryCollectionview.dataSource = self
         
+        registerCell()
+        
+    }
+    private func registerCell() {
+        categoryCollectionview.register(UINib(nibName: CategoryCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
     }
     
 
-    /*
-    // MARK: - Navigation
+ 
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categories.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.identifier, for: indexPath) as! CategoryCollectionViewCell
+        cell.setup(category: categories[indexPath.row])
+        return cell
+    }
+    
+    
 }
