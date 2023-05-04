ec7bb7f0f27c76dff3f58d6274b1b8d1c3379d15
kocakaysegul <aysegulkocak112@gmail.com>
2023-05-04 16:16:11 +0300

    DishLandscapeCollectionViewCell created

---
+++ Yummie/CustomViews/CollectionViewCells/DishLandscapeCollectionViewCell/DishLandscapeCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  DishLandscapeCollectionViewCell.swift
+//  Yummie
+//
+//  Created by Ayşegül Koçak on 2.05.2023.
+//
+
+import UIKit
+import Kingfisher
+
+class DishLandscapeCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: DishLandscapeCollectionViewCell.self)
+
+    @IBOutlet weak var dishImageView: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var caloriesLbl: UILabel!
+    
+    func setup(dish : Dish) {
+        dishImageView.kf.setImage(with: dish.image?.asUrl)
+        titleLbl.text = dish.name
+        descriptionLbl.text = dish.description
+        caloriesLbl.text = dish.formattedCalories
+    }
+    
+}
