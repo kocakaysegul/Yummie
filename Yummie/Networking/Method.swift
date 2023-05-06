01f9a1e0e5d66915babccb4b3e7efe16a8dab59d
kocakaysegul <aysegulkocak112@gmail.com>
2023-05-06 19:43:27 +0300

    Create URLRequest Object for API Call

---
+++ Yummie/Networking/Method.swift
@@ -0,0 +1,15 @@
+//
+//  Method.swift
+//  Yummie
+//
+//  Created by Ayşegül Koçak on 6.05.2023.
+//
+
+import Foundation
+
+enum Method : String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
