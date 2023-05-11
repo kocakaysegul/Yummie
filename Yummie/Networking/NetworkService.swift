b32d174ecec6d6ac50929f40054af9c7018bdbea
kocakaysegul <aysegulkocak112@gmail.com>
2023-05-11 08:37:31 +0300

    request function

--- Yummie/Networking/NetworkService.swift
+++ Yummie/Networking/NetworkService.swift
@@ -10,12 +10,30 @@ import Foundation
 struct NetworkService {
     
     
-    private func request<T: Codable>(route: Route,
+    private func request<T: Decodable>(route: Route,
                                      method: Method,
-                                     parameters: [String : Any]? = nil,
-                                     type: T.Type,
-                                     completion: (Result<T, Error>)-> Void){
-        guard let request = createRequest(route: route, method: method, parameters: parameters) else { return }
+                                     parameters: [String: Any]? = nil,
+                                     completion: @escaping(Result<T, Error>) -> Void) {
+        guard let request = createRequest(route: route, method: method, parameters: parameters) else {
+            completion(.failure(AppError.unknownError))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            var result: Result<Data, Error>?
+            if let data = data {
+                result = .success(data)
+                let responseString = String(data: data, encoding: .utf8) ?? "Could not stringify our data"
+                print("The response is:\n\(responseString)")
+            } else if let error = error {
+                result = .failure(error)
+                print("The error is: \(error.localizedDescription)")
+            }
+            
+            DispatchQueue.main.async {
+                //TODO : decode our result and send back to the user 
+            }
+        }.resume()
     }
     
     
